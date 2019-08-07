96e29a0c50183e107abfd32b29c44650e3fcd845
nbstsh <nbstsh@gmail.com>
2019-08-08 03:19:50 +0900

    Updates

---
+++ precache-manifest.b3b3c3ca1f57f62ee1d2f4f61b1d1780.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3afcaf60a99ecc948e63ab41c022e40",
+    "url": "/ts-react-pokemon/index.html"
+  },
+  {
+    "revision": "b5872bc9ea55cd8bd71e",
+    "url": "/ts-react-pokemon/static/css/main.8adf5da8.chunk.css"
+  },
+  {
+    "revision": "8000ecb2f080dd0fa551",
+    "url": "/ts-react-pokemon/static/js/2.1ea49cef.chunk.js"
+  },
+  {
+    "revision": "b5872bc9ea55cd8bd71e",
+    "url": "/ts-react-pokemon/static/js/main.b347f2cb.chunk.js"
+  },
+  {
+    "revision": "27619dd4c30221fe7b6b",
+    "url": "/ts-react-pokemon/static/js/runtime~main.b78dacc7.js"
+  }
+]);
